a0cf76a21cc640b00aa8ad3063aa02f9c7aad40b
suhaspotluri <54907667+suhaspotluri@users.noreply.github.com>
2021-03-30 21:33:36 -0400

    gitignore

--- secrets.py
+++
@@ -1,8 +0,0 @@
-#fill this in with your credentials and rename to secrets.py
-#make sure to gitignore this file! you will lose points if you share this in your github!
-
-TWITTER_API_KEY="clZ6mPUQIxpcYjxLL96zsgkAB"
-TWITTER_API_SECRET="RFaKdNOX4bcZgMBn30M6Gw1NvnYUF4L3VzI6r0j8VpzkbhwAGV"
-TWITTER_ACCESS_TOKEN="1249108662489993216-uOLq7uLUoiy1A2WFG6Ykcg7wyHXnWv"
-TWITTER_ACCESS_TOKEN_SECRET="Tlq2f9BLLc8LxUW1M3txmO2cxPw7YfU5gazXEjFKSWvZ8"
-
